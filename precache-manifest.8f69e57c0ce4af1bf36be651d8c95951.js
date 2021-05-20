b9b02ee9583a38008fcd1d908e81fadf4873cc22
CCBaxter84 <troyceclark@gmail.com>
2021-05-20 08:50:37 -0600

    Updates

---
+++ precache-manifest.8f69e57c0ce4af1bf36be651d8c95951.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b9e6c6510b15180502fd71561e53d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c7fed72d8b8d542aa4f",
+    "url": "/static/css/main.07a5669e.chunk.css"
+  },
+  {
+    "revision": "7b766193c7cf4d4337a3",
+    "url": "/static/js/2.e74a2c9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e74a2c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7fed72d8b8d542aa4f",
+    "url": "/static/js/main.07c97e85.chunk.js"
+  },
+  {
+    "revision": "75f65fa61937d16649bc",
+    "url": "/static/js/runtime-main.d2b1bba4.js"
+  },
+  {
+    "revision": "adec840d3cf34839b618eee7b8ee90fe",
+    "url": "/static/media/MW.adec840d.gif"
+  },
+  {
+    "revision": "8b37b3d3ec1c71956fbb5411f45fac71",
+    "url": "/static/media/SWJeopardy.8b37b3d3.gif"
+  },
+  {
+    "revision": "bfaf26204bad6c0ae54372c8bb0cb1fa",
+    "url": "/static/media/glenwood4.bfaf2620.jpg"
+  },
+  {
+    "revision": "9ca3fd7c992e9ce4bd39b19b2b00fd78",
+    "url": "/static/media/history-quiz.9ca3fd7c.gif"
+  },
+  {
+    "revision": "fc39f5c4065f3306e9149d44f833cba6",
+    "url": "/static/media/mern-shopping-list.fc39f5c4.gif"
+  },
+  {
+    "revision": "a13ac5c0bf5d540e4999e76d10a680b8",
+    "url": "/static/media/puppies.a13ac5c0.gif"
+  },
+  {
+    "revision": "bcd09c2c82a063a7d9c34031413a75c8",
+    "url": "/static/media/red-october.bcd09c2c.gif"
+  },
+  {
+    "revision": "b62414e8cd0aa37dddb55a0ebd9115e9",
+    "url": "/static/media/rps.b62414e8.gif"
+  },
+  {
+    "revision": "9187372d249dfdc45c9622d593f2d5fc",
+    "url": "/static/media/star-wars-intro.9187372d.gif"
+  },
+  {
+    "revision": "7a81e5d4d97c95fee1aa9f04c923757f",
+    "url": "/static/media/sw-lib.7a81e5d4.gif"
+  },
+  {
+    "revision": "ef601eeb7a6d6ff20351b5eb094614d8",
+    "url": "/static/media/to-do-app.ef601eeb.gif"
+  },
+  {
+    "revision": "34fc52a5b29d4ed248a38946fa0d2660",
+    "url": "/static/media/weather.34fc52a5.gif"
+  }
+]);
